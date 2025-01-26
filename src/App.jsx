ad5081a26528039ad6ded6eb2156cfa5caef1bb3
juwairia1234 <juwairiabaig2003@gmail.com>
2025-01-26 20:55:45 +0530

    to-do-list React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {TodoWrapper} from './components/TodoWrapper'
+
+function App() {
+
+  return (
+    <div>       
+    <TodoWrapper /> 
+    </div>
+  )
+}
+
+export default App
